5717232c04ce9b28f244d8ac782dc413e3264cf1
Thales Rosa <thalescandidorosa@gmail.com>
2022-07-16 14:57:56 -0300

    corrige funcionamento do estencil

--- Sequential.java
+++ Sequential.java
@@ -1,14 +1,30 @@
 import java.awt.Color;
+import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
 
 public class Sequential {
-    public static Color[][] sequential(Color[][] input, int iter) {
+    public static boolean isPointFixed(List<int[]> fixedPoints, int xCoord, int yCoord) {
+        return fixedPoints.stream().filter(o -> o[0] == xCoord && o[1] == yCoord).findFirst().isPresent();
+    }
+
+    public static Color[][] sequential(Color[][] input, int[][] fixedPoints, int iter) {
         int SIZE = input.length;
         int iterCount = 0;
+        // for (int i = 0; i < fixedPoints.length; i = i + 1) {
+        //     String aaaaa = "";
+        //     for (int j = 0; j < fixedPoints[i].length; j = j + 1) {
+        //         aaaaa = aaaaa + " " + String.valueOf(fixedPoints[i][j]);
+        //     }
+        //    System.out.println("Line: " + aaaaa);
+        // }
         while (iter > iterCount) {
             Color[][] aux = new Color[input.length][input[0].length];
-
             for (int i = 1; i < SIZE - 1; i = i + 1) {
                 for (int j = 1; j < SIZE - 1; j = j + 1) {
+                    if (isPointFixed(Arrays.asList(fixedPoints), i, j)) {
+                        continue;
+                    }
                     aux[i][j] = Stencil.avgColor(
                         input[i][j],
                         input[i-1][j],
@@ -21,7 +37,9 @@ public class Sequential {
 
             for (int i = 1; i < SIZE - 1; i = i + 1) {
                 for (int j = 1; j < SIZE - 1; j = j + 1) {
-                    input[i][j] = aux[i][j];
+                    if (aux[i][j] != null) {
+                        input[i][j] = aux[i][j];
+                    }
                 }
             }
 
@@ -31,22 +49,12 @@ public class Sequential {
     }
 
     public static void main(String[] args) {
-        int SIZE = 768;
-        Color[][] map = new Color[SIZE][SIZE];
-
-        for (int i = 0; i < SIZE; i = i + 1) {
-            for (int j = 0; j < SIZE; j = j + 1) {
-                if (i == 0 || j == 0 || i == SIZE - 1 || j == SIZE - 1) {
-                    map[i][j] = new Color(127, 127, 127);
-                } else {
-                    map[i][j] = new Color(0, 0, 0);
-                }
-            }
-        }
-
+        Map<String, Object> result = Stencil.inputFileToMap();
+        Color[][] map = (Color[][]) result.get("map");
+        int[][] fixedPoints = (int[][]) result.get("fixedPoints");
         StopWatch stopWatch = new StopWatch();
-        sequential(map, 1000);
-        Stencil.outputToFile(map);
+        sequential(map, fixedPoints, 10000);
         stopWatch.printElapsedTime();
+        Stencil.outputToFile(map);
     }
 }
--- Stencil.java
+++ Stencil.java
@@ -1,10 +1,32 @@
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
 import java.awt.Color;
 
 public class Stencil {
+    public static Color[][] initMap(int size) {
+        size = size + 2;
+        Color[][] map = new Color[size][size];
+        for (int i = 0; i < size; i = i + 1) {
+            for (int j = 0; j < size; j = j + 1) {
+                if (i == 0 || j == 0 || i == size - 1 || j == size - 1) {
+                    map[i][j] = new Color(127, 127, 127);
+                } else {
+                    map[i][j] = new Color(0, 0, 0);
+                }
+            }
+        }
+        return map;
+    }
+
     public static int calculateAvg(
         int value1,
         int value2,
@@ -12,9 +34,7 @@ public class Stencil {
         int value4,
         int value5
     ) {
-        return (int) Math.round(
-            (float) (value1 + value2 + value3 + value4 + value5) / 5
-        );
+        return (value1 + value2 + value3 + value4 + value5) / 5;
     }
 
     public static Color avgColor(
@@ -33,13 +53,13 @@ public class Stencil {
     }
 
     public static void outputToFile(Color[][] map) {
-        int SIZE = map.length;
+        int size = map.length;
         try {
-            FileOutputStream fileOutput = new FileOutputStream("output.txt");
+            FileOutputStream fileOutput = new FileOutputStream("output.dat");
             BufferedWriter fileWriter = new BufferedWriter(new OutputStreamWriter(fileOutput));
-            for (int i = 0; i < SIZE; i = i + 1) {
+            for (int i = 1; i < size - 1; i = i + 1) {
                 String newLine = "";
-                for (int j = 65; j < 129; j = j + 1) {
+                for (int j = 1; j < size - 1; j = j + 1) {
                     newLine = newLine + colorToString(map[i][j]);
                 }
                 fileWriter.write(newLine);
@@ -52,11 +72,55 @@ public class Stencil {
         }
     }
 
+    public static Map<String, Object> inputFileToMap() {
+        Color[][] map = new Color[1][1];
+        List<int[]> fixedPoints = new ArrayList<int[]>();
+        Map<String, Object> result = new HashMap<String, Object>();
+        try {
+            FileReader fileReader = new FileReader("input.dat");
+            BufferedReader reader = new BufferedReader(fileReader);
+            String currentLine = reader.readLine();
+            int size = Integer.parseInt(currentLine.split(" ")[0]);
+            int nFixedPoints = Integer.parseInt(currentLine.split(" ")[1]);
+            map = initMap(size);
+
+            for (int i = 0; i < nFixedPoints; i = i + 1) {
+                currentLine = reader.readLine();
+                if (currentLine == null) {
+                    break;
+                }
+
+                int xCoord = Integer.parseInt(currentLine.split(" ")[0]);
+                int yCoord = Integer.parseInt(currentLine.split(" ")[1]);
+
+                int red = Integer.parseInt(currentLine.split(" ")[2]);
+                int green = Integer.parseInt(currentLine.split(" ")[3]);
+                int blue = Integer.parseInt(currentLine.split(" ")[4]);
+
+                if (xCoord + 1 > size || yCoord + 1 > size || red > 255 || green > 255 || blue > 255) {
+                    System.out.println("Nao foi possivel incluir o ponto fixo: " + currentLine);
+                    break;
+                }
+                map[xCoord][yCoord] = new Color(red, green, blue);
+                int[] coord = {xCoord, yCoord};
+                fixedPoints.add(coord);
+            }
+            reader.close();
+        } catch (IOException | NumberFormatException  err) {
+            err.printStackTrace();
+        }
+        int[][] fixedPointsArr = new int[fixedPoints.size()][2];
+        Arrays.setAll(fixedPointsArr, fixedPoints::get);
+        result.put("map", map);
+        result.put("fixedPoints", fixedPointsArr);
+        return result;
+    }
+
     public static String colorToString(Color color) {
         return (
-            '<' + String.valueOf(color.getRed()) + ',' +
-            String.valueOf(color.getGreen()) + ',' +
-            String.valueOf(color.getBlue()) + '>'
+            "< " + String.valueOf(color.getRed()) + ", " +
+            String.valueOf(color.getGreen()) + ", " +
+            String.valueOf(color.getBlue()) + " > "
         );
     }
 
